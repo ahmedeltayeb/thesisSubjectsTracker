6806acc9b10a94b0b361041fd636aeaebb8d161a
ahmedeltayeb <hotsauce3737@yahoo.com>
2016-01-16 17:17:11 +0200

    login page

--- thesis/urls.py
+++ thesis/urls.py
@@ -16,10 +16,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from .views import HomePageView
 from .views import SignUpView
+from .views import LoginView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomePageView.as_view() , name = 'home'),
     url(r'^accounts/register/$', SignUpView.as_view() , name = 'signup'),
-    url(r'^login$', , name = ),
+    url(r'^accounts/login/$', LoginView.as_view() , name = 'login'),
 ]
--- thesis/views.py
+++ thesis/views.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import
 
 from django.views import generic
+from django.contrib.auth import authenticate , login , logout 
+from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
 from .forms import RegisterationForm
+from .forms import LoginForm
 
 class HomePageView(generic.TemplateView):
 	template_name = 'home.html'
@@ -12,3 +15,11 @@ class SignUpView (generic.CreateView):
 	form_class = RegisterationForm
 	model = User
 	template_name = 'accounts/signup.html'
+
+
+class LoginForm(generic.FormView):
+	form_class = LoginForm
+	success_url = reverse_lazy('home')
+	template_name = 'accounts/login.html'
+
+	
